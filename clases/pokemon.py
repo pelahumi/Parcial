93c2ce70ae74d7a827dc7d5e8ad758487c6d28b3
Pelayo <phuermij@myuax.com>
2022-04-08 11:45:43 +0200

    constructor

---
+++ clases/pokemon.py
@@ -0,0 +1,14 @@
+class Pokemon():
+    #Constructor
+    def __init__(self, id, nombre, ataques, salud, ataque, defensa):
+        self.id = id #int
+        self.nombre = nombre #str
+        self.ataques = ataques #lista
+        self.salud = salud #int
+        self.ataque = ataque #int
+        self.defensa = defensa #int
+    
+    #Getters y setters
+    
+
+
