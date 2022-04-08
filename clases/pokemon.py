a8ef1d4bd83ebe670db21d2f8917726437838281
Pelayo <phuermij@myuax.com>
2022-04-08 12:15:22 +0200

    Errores

--- clases/pokemon.py
+++ clases/pokemon.py
@@ -1,4 +1,7 @@
 class Pokemon():
+
+    pokedex = []
+
     #Constructor
     def __init__(self, id, nombre, ataques, salud, defensa):
         self.id = id #int
@@ -6,10 +9,37 @@ class Pokemon():
         self.ataques = ataques #diccionario
         self.salud = salud #int
         self.defensa = defensa #int
-    
+
+        #Comprobar
+        if isinstance(id, int):
+            if id not in Pokemon.pokedex:
+                self.id = id
+                Pokemon.pokedex.append(self.id)
+            else:
+                raise ValueError("El pokemon elegido ya está registrado en la pokedex")
+
+        if isinstance(nombre, str):
+            self.nombre = nombre
+        else: 
+            raise TypeError("El nombre tiene que ser un str")
+
+        if isinstance(salud, int):
+            if 1 <= salud <=100:
+                self.salud = salud
+        else:
+            raise ValueError("La salud tiene que ser un número del 1 al 100")
+
+        if isinstance(defensa, int):
+            if 1 <= defensa <= 10:
+                self.defensa = defensa
+        else:
+            raise ValueError("La defensa tiene que ser un número del 1 al 10")
+            
     #Destructor
     def __del__(self):
         print("Se ha eliminado el pokemon")
+    
+    
 
     #Getters y setters
     def get_id(self):
