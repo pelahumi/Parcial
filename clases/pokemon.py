e896f1bc4d0e6163dc31e249360bdc9765a78431
Pelayo <phuermij@myuax.com>
2022-04-08 12:00:47 +0200

    getters

--- clases/pokemon.py
+++ clases/pokemon.py
@@ -7,10 +7,29 @@ class Pokemon():
         self.salud = salud #int
         self.defensa = defensa #int
     
- 
     #Destructor
     def __del__(self):
         print("Se ha eliminado el pokemon")
+
+    #Getters y setters
+    def get_id(self):
+        return self.id
+
+    def get_nombre(self):
+        return self.nombre
+    
+    def get_ataques(self):
+        return self.ataques
+    
+    def get_salud(self):
+        return self.salud
+    
+    def get_defensa(self):
+        return self.defensa
+
+    
+
+        
 
 
 
