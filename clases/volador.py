d739d5bd0274ccb80b0eb222951570019c163a52
Pelayo <phuermij@myuax.com>
2022-04-11 11:54:57 +0200

    pokemon agua

---
+++ clases/volador.py
@@ -0,0 +1,5 @@
+from clases.pokemon import *
+
+class Volador(Pokemon):
+    def __init__(self, id, nombre, tipo,  ataques, salud, ataque, defensa):
+        super.__init__(id, nombre, tipo,  ataques, salud, ataque, defensa)
