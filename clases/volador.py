c2f9767178e419242312adaae5c1df6eb433351c
Pelayo <phuermij@myuax.com>
2022-04-11 12:01:23 +0200

    pokemon volador: constructor y defensa

--- clases/volador.py
+++ clases/volador.py
@@ -1,5 +1,15 @@
 from clases.pokemon import *
+import random
 
 class PokemonVolador(Pokemon):
     def __init__(self, id, nombre, tipo,  ataques, salud, ataque, defensa):
         super.__init__(id, nombre, tipo,  ataques, salud, ataque, defensa)
+    
+    def defense(self, daño):
+        if not isinstance(daño, int):
+            raise TypeError("Los puntos de daño tienen que ser un número")
+
+        print(self.nombre," recibió ",str(daño)," puntos de daño")
+
+        probabilidad_fallo = random.randrange(0, 2)
+        
